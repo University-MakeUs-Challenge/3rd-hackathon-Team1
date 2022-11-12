a13601fc212a6892a05e47c0edf94dabca2f65ee
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-13 01:44:08 +0900

    [FEAT] participateParty Create

--- server/src/main/java/umcteam01/catchcar/web/PartyDao.java
+++ server/src/main/java/umcteam01/catchcar/web/PartyDao.java
@@ -1,4 +1,16 @@
 package umcteam01.catchcar.web;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import umcteam01.catchcar.domain.*;
+
 public class PartyDao {
+
+    private JdbcTemplate jdbcTemplate;
+    public int participateParty(PartyJoinReq partyJoinReq){
+        String participatePartyQuery = "insert into Participate (id, member, status, createdAt, updateAt, active)" +
+                "VALUE(1, 1, \"ACTIVE\", now(), now(), \"ACTIVE\")";
+        Object[] participatePartyParams = new Object[]{partyJoinReq.getUser_id(), partyJoinReq.getParty_id()};
+
+        return this.jdbcTemplate.update(participatePartyQuery, participatePartyParams);
+    }
 }
