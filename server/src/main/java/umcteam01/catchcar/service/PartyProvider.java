5239fc621e7266f721245b391ca091feef798259
jun <jun020216@naver.com>
2022-11-13 04:04:59 +0900

    [DOCS] 문법 오류 해결

--- server/src/main/java/umcteam01/catchcar/service/PartyProvider.java
+++ server/src/main/java/umcteam01/catchcar/service/PartyProvider.java
@@ -29,6 +29,7 @@ public class PartyProvider {
     /**
      * 같은 생성자가 만든 파티그룹이 이미 존재하는 경우
      * - 한 사용자가 만들 수 있는 파티 수는 1개로 제한됨
+     *
      * @param userId
      * @return
      */
@@ -50,15 +51,16 @@ public class PartyProvider {
         }
     }
 
-    public PartyReadResDto getParty(Long party_id) throws BaseException{
+    /*public PartyReadResDto getParty(Long party_id) throws BaseException {
         try {
             PartyReadResDto partyReadResDto = partyDao.getParty(party_id);
             return partyReadResDto;
-        }
-    }
+        } cat
+    }*/
 
     /**
      * 특정 id값을 가지는 유저 조회
+     *
      * @param userId
      * @return
      * @throws BaseException
@@ -71,7 +73,7 @@ public class PartyProvider {
         }
     }
 
-    public List<PartyReadResDto> getPartyList() throws BaseException{
+    public List<PartyReadResDto> getPartyList() throws BaseException {
         try {
             List<PartyReadResDto> partyReadResDto = partyDao.getPartyList();
             return partyReadResDto;
@@ -89,4 +91,5 @@ public class PartyProvider {
             throw new BaseException(DATABASE_ERROR);
         }
     }
+}
 
