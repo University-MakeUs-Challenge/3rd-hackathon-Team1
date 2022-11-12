a32fb8e17b627fc64fc539bb27ec250776821e7c
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-13 01:55:47 +0900

    [FEAT] PartyDao Change - class PartyDao 명시 차원의 @Repository 추가 - setDataSource Function 추가

--- server/src/main/java/umcteam01/catchcar/web/PartyDao.java
+++ server/src/main/java/umcteam01/catchcar/web/PartyDao.java
@@ -1,11 +1,22 @@
 package umcteam01.catchcar.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import umcteam01.catchcar.domain.*;
 
+import javax.sql.DataSource;
+
+@Repository
 public class PartyDao {
 
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     public int participateParty(PartyJoinReq partyJoinReq){
         String participatePartyQuery = "insert into Participate (id, member, status, createdAt, updateAt, active)" +
                 "VALUE(1, 1, \"ACTIVE\", now(), now(), \"ACTIVE\")";
