800c6d09df313f1135f0041d9f5a371e11e6f9fc
jun <jun020216@naver.com>
2022-11-12 20:40:37 +0900

    springboot project settings

---
+++ server/src/main/java/umcteam01/catchcar/CatchCarApplication.java
@@ -0,0 +1,13 @@
+package umcteam01.catchcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchCarApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/umcteam01/catchcar/CatchCarApplicationTests.java
@@ -0,0 +1,13 @@
+package umcteam01.catchcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
